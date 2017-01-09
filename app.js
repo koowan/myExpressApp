f5b93bc0d2466e4ae5c3355c956b653fc900d00e
Chris <20104137@student.glasgowclyde.ac.uk>
2017-01-09 15:57:13 +0000

    page 23/44 of express tutorial

--- app.js
+++ app.js
@@ -4,15 +4,46 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nconf = require('nconf');
+var winston = require('winston');
+var nunjucks = require('nunjucks');
+var ig = require('instagram-node').instagram();
+ig.use({"client_id":"f085d81a778942f9b538976570e9d5fa",
+"client_secret": "f431a981922f4cc0925d1c6d98f71de8"});
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+nunjucks.configure('views', {
+    autoescape:true,
+    express:app
+});
+
+nconf.env("__");
+
+nconf.file("config.json");
+
+nconf.defaults({
+  "http":{
+    "port":3000
+  }
+});
+
+winston.info("Initialised nconf");
+winston.info('HTTP Configuration', nconf.get("http"));
+winston.add(winston.transports.File,{filename:"error.log", level:nconf.get("logger:fileLevel")});
+winston.error("Something went wrong");
+//winston.add(winston.transports.Mail,
+//    {   "to":"email@domain.com",
+//        "username"})
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
